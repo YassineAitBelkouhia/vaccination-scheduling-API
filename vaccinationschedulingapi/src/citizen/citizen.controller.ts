b31611ffbc6ca7d3829cf226859a2ed0192a3a10
YassineAitBelkouhia <77494902+YassineAitBelkouhia@users.noreply.github.com>
2022-02-23 16:38:59 +0100

    added final touches

--- vaccinationschedulingapi/src/citizen/citizen.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('citizen')
-export class CitizenController {
- @Get()
- getCitizens() : any {
-  return [{id : 1, name: "citizen One", address: 'random adress'}];
- }
-}
--- vaccinationschedulingapi/src/citizen/citizen.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CitizenController } from './citizen.controller';
-import { CitizenService } from './citizen.service';
-
-@Module({
-  controllers: [CitizenController],
-  providers: [CitizenService]
-})
-export class CitizenModule {}
